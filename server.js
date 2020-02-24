018b72a5a090ec8fd4a18277cd797ca9aef2c3de
Hazera Munim <hazeramunim@Hazeras-MacBook-Air.local>
2020-02-23 21:24:04 -0500

    routes work

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
+// Set Handlebars
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-
+// Set routes
+var routes = require("./controllers/burgers_Controller.js")
+app.use(routes);
 
 
 // Start our server so that it can begin listening to client requests.
